99bbaf59791612cda2121efcd7a2e5b8dd2dabe4
Ayrton Souza <ayrtonsouza@icloud.com>
2020-08-23 03:37:52 -0300

    enabling husky and lint staged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home({ title = 'React avançado' }: Props) {
         </h1>
 
         <p className="description">
-          Get started by editing <code className="code">pages/index.js</code>
+          Get started by editing <code className="code">pages/index.js </code>
         </p>
 
         <div className="grid">
